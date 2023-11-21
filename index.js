5fecd7a6d7473e06ad9fe054bcb278a77c7f9e82
Jackson.Huang <hysJackson@gmail.com>
2023-11-21 14:25:26 +1100

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 app.use(express.static('public'))
-const port=80
+const port = 80
 app.listen(port, () => {
     console.log(`server runs in http://localhost:${port}`)
 })
